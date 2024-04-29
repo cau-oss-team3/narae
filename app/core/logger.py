e281e2d772a4e11d1689f6f097fc9985be05be7e
Juhyeong Ko <dury.ko@gmail.com>
2024-04-29 23:42:09 +0900

    feat: setup logger

---
+++ app/core/logger.py
@@ -0,0 +1,23 @@
+import logging
+import logging.config
+import yaml
+from os.path import exists, join
+
+
+def set_logging(base_dir: str) -> None:
+    """
+    Sets logging configuration based on YAML files.
+    """
+    config_files = [".logging.dev.yaml", ".logging.yaml"]
+    for config_file in config_files:
+        full_path = join(base_dir, config_file)
+        if exists(full_path):
+            with open(full_path, "rt") as f:
+                config = yaml.safe_load(f.read())
+            logging.config.dictConfig(config)
+            print(f"INFO:     Logging configured using `{config_file}`")
+            return
+
+    # Fallback basic configuration
+    logging.basicConfig(level=logging.DEBUG)
+    print("WARN:      Fallback to basic logging configuration.")
