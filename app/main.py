b5423edd04662c1f9c5422d8eef80541ff27a919
Juhyeong Ko <dury.ko@gmail.com>
2024-04-30 00:18:32 +0900

    feat: when visit root(/) redirect to docs page

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
+from fastapi.responses import RedirectResponse
 import uvicorn
 
 from .bootstrap import create_app
 
+
 app = create_app()
 
-if __name__ == '__main__':
-    uvicorn.run('app.main:app', host='0.0.0.0', port=8000, reload=False)
+
+@app.get("/")
+def index():
+    return RedirectResponse(url="/docs")
+
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
