ed3fa6e5a8a58f3eb416bef2b9748ace2b89afbe
Juhyeong Ko <dury.ko@gmail.com>
2024-04-30 00:59:31 +0900

    fix: logger config for debug

--- app/core/logger.py
+++ app/core/logger.py
@@ -3,12 +3,18 @@ import logging.config
 import yaml
 from os.path import exists, join
 
+from app.settings import settings
+
 
 def set_logging(base_dir: str) -> None:
     """
     Sets logging configuration based on YAML files.
     """
-    config_files = [".logging.dev.yaml", ".logging.yaml"]
+    if settings.env == "prod":
+        config_files = [".logging.yaml"]
+    else:
+        config_files = [".logging.dev.yaml", ".logging.yaml"]
+
     for config_file in config_files:
         full_path = join(base_dir, config_file)
         if exists(full_path):
