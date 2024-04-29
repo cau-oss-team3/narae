9d444e446bb4349b73e69a24458f8a3c813d3208
Juhyeong Ko <dury.ko@gmail.com>
2024-04-30 00:35:15 +0900

    feat: migrate on server startup

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -7,13 +7,22 @@ from app.core.logger import set_logging
 from app.middleware import apply_middleware
 from app.routers import apply_routes
 
+from app.core.database import Base, asyncio_engine  # Ensure you import your Base and async engine
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     set_logging(settings.base_dir)
 
+    # Asynchronously create database tables at startup
+    async with asyncio_engine.begin() as conn:
+        # Create all tables stored in your metadata
+        await conn.run_sync(Base.metadata.create_all)
+
     yield
-    # Clean up code here
+
+    # async with asyncio_engine.begin() as conn:
+    #     # WARN: Drop tables here if needed when app shuts down
+    #     # await conn.run_sync(Base.metadata.drop_all)
 
 
 def create_app() -> FastAPI:
