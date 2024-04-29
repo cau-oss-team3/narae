5b3bcbc6d073821aa6b2318122dba2c1f68f5516
Juhyeong Ko <dury.ko@gmail.com>
2024-04-30 00:34:56 +0900

    fix: item router to use async session

--- app/feats/item/router.py
+++ app/feats/item/router.py
@@ -7,35 +7,37 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from .models import Item
 from .schemas import ItemCreate
 
-router = APIRouter(prefix='/items', tags=['Items'])
+router = APIRouter(prefix="/items", tags=["Items"])
 
 
-@router.get(
-    "/",
-    description="Read items",
-    response_model=List[ItemCreate]
-)
-async def read_items(skip: int = 0, limit: int = 10, session: AsyncSession = Depends(get_async_session)):
-    # items = session.query(Item).offset(skip).limit(limit).all()
-    # items = session.query(Item).all()
-
+@router.get("/", description="Read items", response_model=List[ItemCreate])
+async def read_items(
+    skip: int = 0, limit: int = 10, session: AsyncSession = Depends(get_async_session)
+):
     async with session:
         query = select(Item).offset(skip).limit(limit)
         result = await session.execute(query)
         items = result.scalars().all()
         return items
 
-@router.get("/{item_id}")
-def read_item(item_id: int, db = Depends(get_async_session)):
-    item = db.query(Item).filter(Item.id == item_id).first()
-    if not item:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return {"item": item}
 
-@router.post("")
-def create_item(item: ItemCreate, db = Depends(get_async_session)):
-    db_item = Item(**item.model_dump())
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return {"item": db_item}
+@router.post("/")
+async def create_item(
+    item: ItemCreate, session: AsyncSession = Depends(get_async_session)
+):
+    async with session:
+        new_item = Item(**item.model_dump())
+        session.add(new_item)
+        await session.commit()
+        return new_item
+
+
+@router.get("/{item_id}")
+async def read_item(item_id: int, session: AsyncSession = Depends(get_async_session)):
+    async with session:
+        query = select(Item).filter(Item.id == item_id)
+        result = await session.execute(query)
+        item = result.scalar()
+        if item is None:
+            raise HTTPException(status_code=404, detail="Item not found")
+        return item
