4972f854f22749d5c948ce96724436524b999767
Leejeongin <jeonginlee72@gmail.com>
2024-05-02 15:15:09 +0900

    feat : 안쓰는 모듈들 정리

--- app/feats/auth/schemas.py
+++ app/feats/auth/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, validator, ValidationError, Field
+from pydantic import BaseModel, validator
 from app.core.exceptions import AuthenticationFailedException
 import re
 
