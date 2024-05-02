b573a30900f4ea8d690d2a100ebe1036a31a90ea
Leejeongin <jeonginlee72@gmail.com>
2024-05-02 15:16:05 +0900

    feat : mentor 기본 파일 생성

---
+++ app/feats/mentors/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Integer
+from app.core.database import Base
+
+
+# mentor table 생성 (primary key = id)
+class Mentor(Base):
+    __tablename__ = "Mentor"
+    id = Column(String(50), primary_key=True, index=True, nullable=False)
+    mentor_name = Column(String(45), nullable=False)
+    mentor_field = Column(Integer, nullable=False)
+    user_id = Column(Integer, nullable=False)
+    situation = Column(String(45), nullable=False)
+    task = Column(String(45), nullable=False)
+    intent = Column(String(45), nullable=False)
+    concern = Column(String(45), nullable=False)
+    calibrate = Column(String(45), nullable=False)
---
+++ app/feats/mentors/router.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends, Header
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.core.exceptions import AuthenticationFailedException
+
+from app.settings import settings
+from app.core.database import get_async_session
+
+from .models import Mentor
+from .schemas import STICC, Mentor_detail
+
+# uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+
+router = APIRouter(prefix="/mentors", tags=["mentors"]) #TODO router 추가하기
+
+@router.post("")
+async createMentor(input_mentor_detail : Mentor_detail, )
