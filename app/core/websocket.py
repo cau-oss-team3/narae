bcdcd8d297e0f7d369d59a150efa54b0d2010e02
Juhyeong Ko <dury.ko@gmail.com>
2024-05-06 03:44:29 +0900

    feat: simple websocket manager

---
+++ app/core/websocket.py
@@ -0,0 +1,29 @@
+from fastapi import WebSocket
+
+class WebsocketConnectionManager:
+    """
+    Class for websocket events
+    """
+
+    def __init__(self):
+        self.connections = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.connections.remove(websocket)
+
+    async def send_direct_message(self, message: str, websocket: WebSocket):
+        """
+        Send message to specific websocket
+        """
+        await websocket.send_text(message)
+
+    async def send_broadcast(self, data: str):
+        for connection in self.connections:
+            await connection.send_text(data)
+
+def get_websocket_manager():
+    return WebsocketConnectionManager()
--- app/feats/item/router.py
+++ app/feats/item/router.py
@@ -1,8 +1,12 @@
-from typing import List
-from fastapi import APIRouter, Depends, HTTPException
+from typing import Annotated, List
+from fastapi import APIRouter, Depends, HTTPException, WebSocket
+from fastapi.responses import HTMLResponse
 from sqlalchemy import select
 from app.core.database import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.websocket import WebsocketConnectionManager, get_websocket_manager
+from app.feats.item.depends import get_cookie_or_token, test_page
 
 from .models import Item
 from .schemas import ItemCreate
@@ -32,6 +36,11 @@ async def create_item(
         return new_item
 
 
+@router.get("/ws")
+async def get_test_page():
+    return HTMLResponse(content=test_page, status_code=200)
+
+
 @router.get("/{item_id}")
 async def read_item(item_id: int, session: AsyncSession = Depends(get_async_session)):
     async with session:
@@ -41,3 +50,25 @@ async def read_item(item_id: int, session: AsyncSession = Depends(get_async_sess
         if item is None:
             raise HTTPException(status_code=404, detail="Item not found")
         return item
+
+
+@router.websocket("/{item_id}/ws")
+async def websocket_endpoint(
+    *,
+    websocket: WebSocket,
+    item_id: str,
+    q: int | None = None,
+    cookie_or_token: Annotated[str, Depends(get_cookie_or_token)],
+    manager: WebsocketConnectionManager = Depends(get_websocket_manager),
+):
+    await manager.connect(websocket)
+    while True:
+        data = await websocket.receive_text()
+        await manager.send_direct_message(
+            f"Session cookie or query token value is: {cookie_or_token}", websocket
+        )
+        if q is not None:
+            await manager.send_direct_message(f"Query parameter q is: {q}", websocket)
+        await manager.send_direct_message(
+            f"Message text was: {data}, for item ID: {item_id}", websocket
+        )
