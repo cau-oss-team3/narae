e2e4101927174f85b00a66fe39184d06a6322739
Juhyeong Ko <dury.ko@gmail.com>
2024-06-10 04:51:01 +0900

    feat: migrate mentor2

--- app/feats/mentors/models.py
+++ app/feats/mentors/models.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, ForeignKey, String, Integer
+from datetime import datetime
+
+from sqlalchemy import Column, ForeignKey, String, Integer, Text, BigInteger
 from sqlalchemy.orm import relationship
+from typing_extensions import deprecated
 
 from app.core.database import Base
 
 
-# mentor table 생성 (primary key = id)
 class Mentor(Base):
     __tablename__ = "Mentor"
     id = Column(String(100), primary_key=True, index=True, nullable=False)
@@ -24,6 +26,7 @@ class Mentor2(Base):
     mentor_field = Column(Integer, nullable=False)
 
     user_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    user = relationship("User", back_populates="mentors")
 
     situation = Column(String(256), nullable=False)
     task = Column(String(256), nullable=False)
@@ -31,4 +34,29 @@ class Mentor2(Base):
     concern = Column(String(256), nullable=False)
     calibrate = Column(String(256), nullable=False)
 
-    user = relationship("User", back_populates="mentors")
+    curriculum = Column(Text)
+    curriculum_phase = Column(Text)
+
+    action_id = Column(Integer, ForeignKey("Action.id"))
+    actions = relationship("Action", back_populates="mentor")
+
+
+class Action(Base):
+    __tablename__ = "Action"
+    mentor_id = Column(Integer, ForeignKey('Mentor2.id'), nullable=False)
+    mentor = relationship("Mentor2", back_populates="actions")
+
+    action = Column(Text, nullable=False)
+    is_active = Column(Integer, nullable=False)
+    
+    created_at = Column(
+        BigInteger,
+        default=lambda: int(datetime.now().timestamp() * 1000),
+        nullable=False,
+    )
+    updated_at = Column(
+        BigInteger,
+        default=lambda: int(datetime.now().timestamp() * 1000),
+        onupdate=lambda: int(datetime.now().timestamp() * 1000),
+        nullable=False,
+    )
